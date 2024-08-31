2f443a5ca9705df013354f897f23278bf4d1a26c
Hemanath031 <dharanishhemanath@gmail.com>
2024-08-31 13:39:23 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,15 @@
+package electricitybllmanagement;
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "root"); // replace the password according to your account
+            s = c.createStatement();
+        } catch (Exception e) {
+        }
+    }
+}
